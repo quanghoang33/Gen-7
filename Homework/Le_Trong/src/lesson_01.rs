db856f9570603dd81a111bac733052cf2f20d712
tronglt <trongle1711@gmail.com>
2023-02-14 15:28:12 +0700

    Add solution for problem 03.

--- Homework/Le_Trong/src/lesson_01.rs
+++ Homework/Le_Trong/src/lesson_01.rs
@@ -12,16 +12,12 @@
  *  Space Complexity: O(1)
  */
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    return if nums.len() == 0 {
-        nums[0]
-    } else {
-        let mut result = 0;
-        for i in 0..(nums.len()) {
-            result = result ^ nums[i];
-        }
+    let mut result = nums[0];
+    for i in 1..(nums.len()) {
+        result = result ^ nums[i];
+    }
 
-        result
-    };
+    return result;
 }
 
 /**
@@ -41,6 +37,40 @@ pub fn hamming_weight(n: u32) -> i32 {
     };
 }
 
+struct NumArray {
+    nums: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        return NumArray { nums };
+    }
+
+    /**
+     * Dùng vòng lặp.
+     *
+     * Runtime Complexity: O(n * m) với n là số queries, và m là nums.len().
+     * Space Complexity: O(1).
+     */
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        let mut result = 0;
+        for i in left..=right {
+            result += self.nums[i as usize];
+        }
+        return result;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * let obj = NumArray::new(nums);
+ * let ret_1: i32 = obj.sum_range(left, right);
+ */
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,5 +87,14 @@ mod tests {
         assert_eq!(hamming_weight(0b00000000000000000000000000001011), 3);
         assert_eq!(hamming_weight(0b00000000000000000000000010000000), 1);
         assert_eq!(hamming_weight(0b11111111111111111111111111111101), 31);
+    }
+
+    #[test]
+    fn test_range_sum_query_immutable() {
+        let num_array = NumArray::new(vec![-2, 0, 3, -5, 2, -1]);
+
+        assert_eq!(num_array.sum_range(0, 2), 1);
+        assert_eq!(num_array.sum_range(2, 5), -1);
+        assert_eq!(num_array.sum_range(0, 5), -3);
     }
 }
