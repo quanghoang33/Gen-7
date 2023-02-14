ac1083101c8366a142c586d7c5c8d2fae97e01f2
tronglt <trongle1711@gmail.com>
2023-02-14 17:07:28 +0700

    Refactor: move each solutions into its own file.

--- Homework/Le_Trong/src/main.rs
+++ Homework/Le_Trong/src/main.rs
@@ -1,6 +1,8 @@
-mod lesson_01;
+pub mod lesson_01;
 
-use lesson_01::*;
+use lesson_01::hamming_weight::hamming_weight;
+use lesson_01::range_sum_query_immutable::*;
+use lesson_01::single_number::single_number;
 
 fn main() {
     println!(
@@ -12,4 +14,9 @@ fn main() {
         "Hamming weight of 00000000000000000000000000001011: {}",
         hamming_weight(0b00000000000000000000000000001011)
     );
+
+    println!(
+        "Range sum query immutable of [-2, 0, 3, -5, 2, -1]: {}",
+        (NumArray::new(vec![-2, 0, 3, -5, 2, -1])).sum_range(0, 2)
+    )
 }
