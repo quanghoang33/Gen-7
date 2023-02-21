22c213a8027bf918daa0239ee942367f0b2a0e7c
codeeMadness <codee.madness@gmail.com>
2023-02-21 23:15:29 +0700

    submit 1 and 3

--- Homework/Hang_Nguyen/lesson_2/1.sort-list.py
+++ Homework/Hang_Nguyen/lesson_2/1.sort-list.py
@@ -1,1 +1,72 @@
 # https://leetcode.com/problems/sort-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+
+    # HELPER
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left, right):
+        tail = dummy = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+        return dummy.next
+    
+    def printList(self, head): 
+        current = head
+        while current:
+            print(current.val)
+            current = current.next
+
+    def sortList(self, head):
+        if head is None or head.next is None:
+            return head
+        
+        # split list at middle
+        left = head
+        right = self.getMid(head)
+        temp = right.next
+        right.next = None
+        right = temp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+    
+
+
+# TEST CASE
+solution = Solution()
+
+head = ListNode(4, ListNode(2, ListNode(1, ListNode(3))))
+head = solution.sortList(head)
+print("TEST 1 is ")
+solution.printList(head)
+
+head = ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0)))))
+head = solution.sortList(head)
+print("TEST 2 is ")
+solution.printList(head)
+
+head = ListNode()
+head = solution.sortList(head)
+print("TEST 3 is ")
+solution.printList(head)
--- Homework/Hang_Nguyen/lesson_2/3.sliding-window-maximum.py
+++ Homework/Hang_Nguyen/lesson_2/3.sliding-window-maximum.py
@@ -1,1 +1,47 @@
 # https://leetcode.com/problems/sliding-window-maximum/
+
+
+import collections
+
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        res = []
+        queue = collections.deque()
+        left = right = 0
+
+        while right < len(nums):
+            # pop index of smaller value
+            while queue and nums[queue[-1]] < nums[right]:
+                queue.pop()
+            queue.append(right)
+
+            # remove index of left value
+            if left > queue[0]:
+                queue.popleft()
+
+            if right + 1 >= k:
+                res.append(nums[queue[0]])
+                left += 1
+            right += 1
+        
+        return res
+
+# TEST CASE
+solution = Solution()
+
+nums = [1,3,-1,-3,5,3,6,7] 
+k = 3
+# print("TEST 1 is " + str(solution.maxSlidingWindow(nums, k)))
+
+nums = [1] 
+k = 1
+# print("TEST 2 is " + str(solution.maxSlidingWindow(nums, k)))
+
+nums = [1,-1]
+k = 1
+# print("TEST 3 is " + str(solution.maxSlidingWindow(nums, k)))
+
+nums = [3,1,-1,-3] 
+k = 2
+print("TEST 4 is " + str(solution.maxSlidingWindow(nums, k)))
