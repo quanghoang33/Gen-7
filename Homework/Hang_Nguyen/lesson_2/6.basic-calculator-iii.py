650096adc4bc47d46922bea8f3d29d70ffd80a79
codeeMadness <codee.madness@gmail.com>
2023-02-27 22:12:48 +0700

    submit quiz 6

--- Homework/Hang_Nguyen/lesson_2/6.basic-calculator-iii.py
+++ Homework/Hang_Nguyen/lesson_2/6.basic-calculator-iii.py
@@ -1,53 +1,53 @@
 # https://leetcode.com/problems/basic-calculator-iii/
 
 class Solution:
-    def operation(self, op, second, first):
-        if op == "+":
+    def cal(self, operator, second, first):
+        if operator == "+":
             return first + second
-        elif op == "-":
+        elif operator == "-":
             return first - second
-        elif op == "*":
+        elif operator == "*":
             return first * second
-        elif op == "/":
+        elif operator == "/":
             return first // second
     
-    def precedence(self, current_op, op_from_ops):
-        if op_from_ops == "(" or op_from_ops == ")":
+    def shouldCalculatePrecedence(self, currentOperator, latestOperator):
+        if latestOperator == "(" or latestOperator == ")":
             return False
-        if (current_op == "*" or current_op == "/") and (op_from_ops == "+" or op_from_ops == "-"):
+        if (currentOperator == "*" or currentOperator == "/") and (latestOperator == "+" or latestOperator == "-"):
             return False
         return True
 
     def calculate(self, s):
         if not s: 
             return 0
-        nums, ops = [], []
+        nums, operators = [], []
         i = 0
         while i < len(s):
-            c = s[i]
-            if c == " ":
+            char = s[i]
+            if char == " ":
                 i += 1
                 continue
-            elif c.isdigit():
-                num = int(c)
+            elif char.isdigit():
+                num = int(char)
                 while i < len(s) - 1 and s[i + 1].isdigit():
                     num = num * 10 + int(s[i + 1])
                     i += 1
                 nums.append(num)
-            elif c == "(":
-                ops.append(c)
-            elif c == ")":
-                while ops[-1] != "(":
-                    nums.append(self.operation(ops.pop(), nums.pop(), nums.pop()))
-                ops.pop()
-            elif c in ["+", "-", "*", "/"]:
-                while len(ops) != 0 and self.precedence(c, ops[-1]):
-                    nums.append(self.operation(ops.pop(), nums.pop(), nums.pop()))
-                ops.append(c)
+            elif char == "(":
+                operators.append(char)
+            elif char == ")":
+                while operators[-1] != "(":
+                    nums.append(self.cal(operators.pop(), nums.pop(), nums.pop()))
+                operators.pop()
+            elif char in ["+", "-", "*", "/"]:
+                while len(operators) != 0 and self.shouldCalculatePrecedence(char, operators[-1]):
+                    nums.append(self.cal(operators.pop(), nums.pop(), nums.pop()))
+                operators.append(char)
             i += 1
 
-        while len(ops) > 0:
-            nums.append(self.operation(ops.pop(), nums.pop(), nums.pop()))
+        while len(operators) > 0:
+            nums.append(self.cal(operators.pop(), nums.pop(), nums.pop()))
 
         return nums.pop()
 
