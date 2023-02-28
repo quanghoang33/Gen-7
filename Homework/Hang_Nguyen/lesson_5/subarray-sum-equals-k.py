1147b95d9248844ec71676ed903467983a53c4f8
codeeMadness <codee.madness@gmail.com>
2023-02-28 17:27:14 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_5/subarray-sum-equals-k.py
+++ Homework/Hang_Nguyen/lesson_5/subarray-sum-equals-k.py
@@ -1,1 +1,27 @@
 # https://leetcode.com/problems/subarray-sum-equals-k/
+class Solution:
+    def subarraySum(self, nums, k):
+        res = 0
+        currentSum = 0
+        prefixSum = { 0 : 1 }
+
+        for num in nums:
+            currentSum += num
+            diff = currentSum - k
+            res += prefixSum.get(diff, 0)
+            prefixSum[currentSum] = 1 + prefixSum.get(currentSum, 0)
+        
+        return res
+        
+               
+
+# TEST CASE
+solution = Solution()
+
+nums = [1,1,1] 
+k = 2
+# print(solution.subarraySum(nums, k))
+
+nums = [0,3,1,2,3]
+k = 3
+print(solution.subarraySum(nums, k))
