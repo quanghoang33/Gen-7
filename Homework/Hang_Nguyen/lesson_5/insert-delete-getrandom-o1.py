e29a11ead74c61c0041618a85e8db251f52f7ed4
codeeMadness <codee.madness@gmail.com>
2023-02-28 20:32:13 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_5/insert-delete-getrandom-o1.py
+++ Homework/Hang_Nguyen/lesson_5/insert-delete-getrandom-o1.py
@@ -1,1 +1,35 @@
 # https://leetcode.com/problems/insert-delete-getrandom-o1/
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.map = {}
+        
+
+    def insert(self, val: int) -> bool:
+        if val in self.map:
+            return False
+        self.map[val] = val
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.map:
+            self.map.pop(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        vals = list(self.map.values())
+        return random.choice(vals)
+
+#TEST CASE
+randomizedSet = RandomizedSet()
+print(randomizedSet.insert(1))
+print(randomizedSet.remove(2))
+print(randomizedSet.insert(2))
+print(randomizedSet.getRandom())
+print(randomizedSet.remove(1))
+print(randomizedSet.insert(2))
+print(randomizedSet.getRandom())
+
