5c986cd3e3999f6e789f2677ec1b5a9857e170b2
codeeMadness <codee.madness@gmail.com>
2023-03-15 23:35:40 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_6/3.max-area-of-island.py
+++ Homework/Hang_Nguyen/lesson_6/3.max-area-of-island.py
@@ -1,1 +1,34 @@
 # https://leetcode.com/problems/max-area-of-island/
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if r < 0 or r == rows or c < 0 or c == cols or grid[r][c] == 0 or (r,c) in visit:
+                return 0
+            visit.add((r,c))
+            return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
+        
+        area = 0
+        for r in range(rows):
+            for c in range(cols):
+                area = max(area, dfs(r,c))
+        return area
+
+# TEST CASE
+solution = Solution()
+
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,1,1,0,1,0,0,0,0,0,0,0,0],
+[0,1,0,0,1,1,0,0,1,0,1,0,0],
+[0,1,0,0,1,1,0,0,1,1,1,0,0],
+[0,0,0,0,0,0,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+print("TEST 1 is " + str(solution.maxAreaOfIsland(grid)))
+
+grid = [[0,0,0,0,0,0,0,0]]
+print("TEST 2 is " + str(solution.maxAreaOfIsland(grid)))
