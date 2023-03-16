f851f624d511a37d41ae4336b7b17876031d41f1
codeeMadness <codee.madness@gmail.com>
2023-03-16 22:17:43 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_6/6.minimum-genetic-mutation.py
+++ Homework/Hang_Nguyen/lesson_6/6.minimum-genetic-mutation.py
@@ -1,1 +1,37 @@
 # https://leetcode.com/problems/minimum-genetic-mutation/
+
+from collections import deque
+
+class Solution():
+    def minMutation(self, startGene, endGene, bank):
+        def checkNeighbor(a,b):
+            return sum([1 for i in range(len(a)) if a[i]!=b[i]]) == 1
+        
+        q = deque([startGene])
+        visited = set()
+        mutations = 0
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur == endGene:
+                    return mutations
+                if cur not in visited:
+                    visited.add(cur)
+                    for nei in bank:
+                        if checkNeighbor(cur,nei):
+                            q.append(nei)
+            mutations += 1
+        return -1
+
+# TEST CASE
+solution = Solution()
+
+startGene = "AACCGGTT"
+endGene = "AACCGGTA"
+bank = ["AACCGGTA"]
+print(solution.minMutation(startGene, endGene, bank))
+
+startGene = "AACCGGTT"
+endGene = "AAACGGTA"
+bank = ["AACCGGTA","AACCGCTA","AAACGGTA"]
+print(solution.minMutation(startGene, endGene, bank))
