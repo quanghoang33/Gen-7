2294dcde0c3efd6a72c08b6868423cf527abf332
codeeMadness <codee.madness@gmail.com>
2023-03-28 17:39:07 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_8/1.max-consecutive-ones-iii.py
+++ Homework/Hang_Nguyen/lesson_8/1.max-consecutive-ones-iii.py
@@ -1,1 +1,21 @@
 # https://leetcode.com/problems/max-consecutive-ones-iii/
+class Solution():
+    def longestOnes(self, nums, k):
+        i = 0
+        for j in range(len(nums)):
+            k -= 1 - nums[j]
+            if k < 0:
+                k += 1 - nums[i]
+                i += 1
+        return j - i + 1
+
+# TEST CASE
+solution = Solution()
+
+nums = [1,1,1,0,0,0,1,1,1,1,0]
+k = 2
+print('Test 1 is ' + str(solution.longestOnes(nums, k)))
+
+nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+k = 3
+print('Test 2 is ' + str(solution.longestOnes(nums, k)))
--- Homework/Hang_Nguyen/lesson_8/2.minimum-window-substring.py
+++ Homework/Hang_Nguyen/lesson_8/2.minimum-window-substring.py
@@ -1,1 +1,35 @@
 # https://leetcode.com/problems/minimum-window-substring/
+import collections
+
+class Solution(object):
+    def minWindow(self, s, t):
+        target_count = collections.Counter(t)
+        start, end = 0, 0
+        min_window = ''
+        target_len = len(t)
+        
+        for end in range(len(s)):
+            if target_count[s[end]] > 0:
+                target_len -= 1
+                
+            target_count[s[end]] -= 1
+            while target_len == 0:
+                window_len = end - start + 1
+                
+                if not min_window or window_len < len(min_window):
+                    min_window = s[start:end + 1]
+                    
+                target_count[s[start]] += 1
+                if target_count[s[start]] > 0:
+                    target_len += 1
+                start += 1
+                
+        return min_window
+
+# TEST CASE
+solution = Solution()
+
+s = "ADOBECODEBANC"
+t = "ABC"
+
+print(solution.minWindow(s, t))
