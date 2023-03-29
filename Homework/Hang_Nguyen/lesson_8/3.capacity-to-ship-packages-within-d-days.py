c57e66e108885228e25177b0759b8c2ecb1b1d81
codeeMadness <codee.madness@gmail.com>
2023-03-29 22:38:50 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_8/3.capacity-to-ship-packages-within-d-days.py
+++ Homework/Hang_Nguyen/lesson_8/3.capacity-to-ship-packages-within-d-days.py
@@ -1,1 +1,23 @@
 # https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+class Solution():
+    def shipWithinDays(self, weights, days):
+        low = max(weights)
+        high = sum(weights)
+        while low < high:
+            mid, need, cur = low + (high-low)//2, 1, 0
+            for w in weights:
+                if cur + w > mid:
+                    need += 1
+                    cur = 0
+                cur += w
+            if need > days: low = mid + 1
+            else: high = mid
+        return low
+
+# TEST CASE
+solution = Solution()
+
+weights = [1,2,3,4,5,6,7,8,9,10]
+days = 5
+
+print(str(solution.shipWithinDays(weights, days)))
--- Homework/Hang_Nguyen/lesson_8/4.search-in-rotated-sorted-array-ii.py
+++ Homework/Hang_Nguyen/lesson_8/4.search-in-rotated-sorted-array-ii.py
@@ -1,1 +1,52 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+class Solution(object):
+    def search(self, nums, target):
+        # If the length of the given array list is 1, then check the first element and return accordingly
+        if len(nums)==1:
+            if nums[0]!=target:
+                return False
+            else:
+                return True
+
+        left=0
+        right=len(nums)-1
+        # binary search 
+        while(left<=right):
+
+            # shifting to remove duplicate elements
+            while left<right and nums[left] == nums[left+1]:
+                left+=1
+            while left<right and nums[right] == nums[right-1]:
+                right-=1
+
+            # step 1 calculate the mid    
+            mid=(left+right)//2
+
+            #step 2
+            if nums[mid]==target:
+                return True
+
+            #step 3
+            elif nums[left]<=nums[mid]:
+                if nums[mid]>=target and nums[left]<=target:
+                    right=mid-1
+                else:
+                    left=mid+1
+
+            # step 4
+            else:
+                if target>=nums[mid] and target<=nums[right]:
+                    left=mid+1
+                else:
+                    right=mid-1
+
+        # step 5
+        return False
+
+# TEST CASE
+solution = Solution()
+
+nums = [2,5,6,0,0,1,2] 
+target = 0
+
+print(solution.search(nums, target))
