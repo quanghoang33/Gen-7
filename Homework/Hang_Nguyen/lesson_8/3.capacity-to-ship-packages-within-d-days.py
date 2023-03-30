a42fd32ba2b027fce4ec4d6b7fad77ce08f1eb5e
codeeMadness <codee.madness@gmail.com>
2023-03-30 11:40:34 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_8/3.capacity-to-ship-packages-within-d-days.py
+++ Homework/Hang_Nguyen/lesson_8/3.capacity-to-ship-packages-within-d-days.py
@@ -1,23 +1,36 @@
 # https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 class Solution():
     def shipWithinDays(self, weights, days):
-        low = max(weights)
-        high = sum(weights)
-        while low < high:
-            mid, need, cur = low + (high-low)//2, 1, 0
+        capacityLow = max(weights)
+        capacityHigh = sum(weights)
+        while capacityLow < capacityHigh:
+            middle, need, current = capacityLow + (capacityHigh - capacityLow)//2, 1, 0
+
             for w in weights:
-                if cur + w > mid:
+                if current + w > middle:
                     need += 1
-                    cur = 0
-                cur += w
-            if need > days: low = mid + 1
-            else: high = mid
-        return low
+                    current = 0
+                current += w
+
+            if need > days: 
+                capacityLow = middle + 1
+            else: 
+                capacityHigh = middle
+                
+        return capacityLow
 
 # TEST CASE
 solution = Solution()
 
-weights = [1,2,3,4,5,6,7,8,9,10]
+weights = [1,2,3,4,5,6,7,8,9,10] # middle = 32
 days = 5
 
+print(str(solution.shipWithinDays(weights, days)))
+
+weights = [3,2,2,4,1,4] 
+days = 3
+print(str(solution.shipWithinDays(weights, days)))
+
+weights = [1,2,3,1,1] 
+days = 4
 print(str(solution.shipWithinDays(weights, days)))
