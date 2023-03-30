1e7b1f7abee3ac3cc800e98cb68bda1de0a13692
codeeMadness <codee.madness@gmail.com>
2023-03-30 22:46:07 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_8/5.find-minimum-in-rotated-sorted-array.py
+++ Homework/Hang_Nguyen/lesson_8/5.find-minimum-in-rotated-sorted-array.py
@@ -1,1 +1,25 @@
 # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+class Solution(object):
+    def findMin(self, nums):
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
+
+
+# TEST CASE
+solution = Solution()
+
+nums = [3,4,5,1,2]
+
+print(solution.findMin(nums))
+
+nums = [4,5,6,0,1,2,3]
+print(solution.findMin(nums))
+
+nums = [11,13,15,17]
+print(solution.findMin(nums))
