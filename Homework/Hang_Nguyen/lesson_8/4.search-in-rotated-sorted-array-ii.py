022108a3b8517c3647ae97ac8da03fe515a27287
codeeMadness <codee.madness@gmail.com>
2023-03-30 22:13:00 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_8/4.search-in-rotated-sorted-array-ii.py
+++ Homework/Hang_Nguyen/lesson_8/4.search-in-rotated-sorted-array-ii.py
@@ -1,44 +1,35 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 class Solution(object):
     def search(self, nums, target):
-        # If the length of the given array list is 1, then check the first element and return accordingly
-        if len(nums)==1:
-            if nums[0]!=target:
-                return False
-            else:
+        if len(nums) == 1:
+            return nums[0] != target
+
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            
+            # remove duplicate ones
+            while left < right and nums[left] == nums[left+1]:
+                left += 1
+            while left < right and nums[right] == nums[right-1]:
+                right -= 1
+  
+            mid= (left+right)//2
+
+            if nums[mid] == target:
                 return True
 
-        left=0
-        right=len(nums)-1
-        # binary search 
-        while(left<=right):
-
-            # shifting to remove duplicate elements
-            while left<right and nums[left] == nums[left+1]:
-                left+=1
-            while left<right and nums[right] == nums[right-1]:
-                right-=1
-
-            # step 1 calculate the mid    
-            mid=(left+right)//2
-
-            #step 2
-            if nums[mid]==target:
-                return True
-
-            #step 3
-            elif nums[left]<=nums[mid]:
-                if nums[mid]>=target and nums[left]<=target:
-                    right=mid-1
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target and target <= nums[mid] :
+                    right = mid-1
                 else:
-                    left=mid+1
+                    left = mid+1
 
-            # step 4
             else:
-                if target>=nums[mid] and target<=nums[right]:
-                    left=mid+1
+                if nums[mid] <= target and target <= nums[right]:
+                    left = mid+1
                 else:
-                    right=mid-1
+                    right = mid-1
 
         # step 5
         return False
@@ -47,6 +38,6 @@ class Solution(object):
 solution = Solution()
 
 nums = [2,5,6,0,0,1,2] 
-target = 0
+target = 5
 
 print(solution.search(nums, target))
