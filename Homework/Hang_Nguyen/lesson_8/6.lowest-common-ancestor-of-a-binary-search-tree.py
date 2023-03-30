7e7877665ea7093fed194e721725eec680fc3498
codeeMadness <codee.madness@gmail.com>
2023-03-30 22:52:00 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_8/6.lowest-common-ancestor-of-a-binary-search-tree.py
+++ Homework/Hang_Nguyen/lesson_8/6.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,1 +1,24 @@
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        small = min(p.val, q.val)
+        large = max(p.val, q.val)
+        while root:
+            if root.val > large:  # p, q belong to the left subtree
+                root = root.left
+            elif root.val < small:  # p, q belong to the right subtree
+                root = root.right
+            else:  # Now, small <= root.val <= large -> This is the LCA between p and q
+                return root
+        return None
+
+
+# TEST CASE
+solution = Solution()
+
