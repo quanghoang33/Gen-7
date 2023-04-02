a1ef7fd6ad0f0f741d98507cf1a228ce2e8879fa
tronglt <trongle1711@gmail.com>
2023-04-02 16:30:51 +0700

    Add solution for problem 05.

--- Homework/Le_Trong/src/lesson_08_09/mod.rs
+++ Homework/Le_Trong/src/lesson_08_09/mod.rs
@@ -1,5 +1,6 @@
 pub mod capacity_to_ship_packges_within_d_days;
 pub mod find_minimum_in_rotated_sorted_array;
+pub mod lowest_common_ancestor;
 pub mod max_consecutive_ones_iii;
 pub mod minimum_window_substring;
 pub mod search_in_rotated_sorted_array_ii;
