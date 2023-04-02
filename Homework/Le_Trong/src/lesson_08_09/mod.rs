d7d3fc8dd2fac5d28143eb07273689afac8902d5
tronglt <trongle1711@gmail.com>
2023-04-02 15:00:04 +0700

    Add solution for problem 05.

--- Homework/Le_Trong/src/lesson_08_09/mod.rs
+++ Homework/Le_Trong/src/lesson_08_09/mod.rs
@@ -1,4 +1,5 @@
 pub mod capacity_to_ship_packges_within_d_days;
+pub mod find_minimum_in_rotated_sorted_array;
 pub mod max_consecutive_ones_iii;
 pub mod minimum_window_substring;
 pub mod search_in_rotated_sorted_array_ii;
